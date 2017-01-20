835d4e28b1915cbb9a8609f1db43603dcf58d235
Joseph Walton-Rivers <joseph@webpigeon.me.uk>
2017-01-20 19:23:34 +0000

    Initial commit

---
+++ src/main/java/com/fossgalaxy/games/ggj2017/App.java
@@ -0,0 +1,13 @@
+package com.fossgalaxy.games.ggj2017;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
