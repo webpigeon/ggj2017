60c872e9ee2d00ecf2e48e39b8bf6b15cf39b708
Joseph Walton-Rivers <joseph@webpigeon.me.uk>
2017-01-21 10:54:10 +0000

    game loop

--- src/main/java/com/fossgalaxy/games/ggj2017/App.java
+++ src/main/java/com/fossgalaxy/games/ggj2017/App.java
@@ -1,13 +1,40 @@
 package com.fossgalaxy.games.ggj2017;
 
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws InterruptedException {
+        System.out.println("Hello World!");
+
+
+        Frame frame = new Frame();
+        frame.setPreferredSize(new Dimension(800, 600));
+        Canvas canvas = new Canvas();
+
+        frame.add(canvas);
+        frame.pack();
+        frame.setVisible(true);
+
+        canvas.createBufferStrategy(2);
+        BufferStrategy bs = canvas.getBufferStrategy();
+
+        boolean running = true;
+
+        while (running) {
+            Graphics g = bs.getDrawGraphics();
+
+            g.setColor(Color.BLACK);
+            g.drawRect(0, 0, 10, 10);
+
+            bs.show();
+
+            Thread.sleep(1000/50);
+        }
     }
 }
