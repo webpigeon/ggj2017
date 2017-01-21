d343788cd080c45a92f538ee1157b7bb7a3c4d40
Piers Williams <solar_1992@hotmail.co.uk>
2017-01-21 21:10:31 +0000

    Added range check

--- src/main/java/com/fossgalaxy/games/ggj2017/world/VectorUtils.java
+++ src/main/java/com/fossgalaxy/games/ggj2017/world/VectorUtils.java
@@ -21,4 +21,20 @@ public class VectorUtils {
 
         return (float) pd;
     }
+
+    /**
+     *
+     * @param p Position of vortex
+     * @param m Position of max range in correct direction
+     * @param s Position of the ship
+     * @param theta The max allowed angle. Will allow anything +/- theta/2
+     * @return
+     */
+    public static boolean thetaCheck(Vec2 p, Vec2 m, Vec2 s, double theta){
+        Vec2 ps = s.sub(p);
+        Vec2 pm = m.sub(p);
+        double mps = Math.acos(Vec2.dot(ps, pm) / (ps.length() * pm.length()));
+
+        return (theta / 2 > Math.abs(mps));
+    }
 }
